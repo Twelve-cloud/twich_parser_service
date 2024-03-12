1613ac566c3d5340b190bea7a6b1293d78cf3c08
Twelve <kana.suzucki@gmail.com>
2024-03-12 17:11:11 +0300

    fix: common logger interface

--- tlparser/src/common/interfaces/logger.py
+++ tlparser/src/common/interfaces/logger.py
@@ -3,24 +3,30 @@ logger.py: File, containing logger interface.
 """
 
 
-from interface import Interface
+from abc import ABC as Interface, abstractmethod
 
 
 class ILogger(Interface):
+    @abstractmethod
     def _configure_logger(self, name: str) -> None:
         raise NotImplementedError
 
+    @abstractmethod
     def info(self, message: str) -> None:
         raise NotImplementedError
 
+    @abstractmethod
     def debug(self, message: str) -> None:
         raise NotImplementedError
 
+    @abstractmethod
     def warning(self, message: str) -> None:
         raise NotImplementedError
 
+    @abstractmethod
     def error(self, message: str) -> None:
         raise NotImplementedError
 
+    @abstractmethod
     def critical(self, message: str) -> None:
         raise NotImplementedError
