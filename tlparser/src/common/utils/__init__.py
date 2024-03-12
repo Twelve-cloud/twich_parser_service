8d7bfff7f0b430e645c080e1456467cafb0922cb
Twelve <kana.suzucki@gmail.com>
2024-03-12 22:49:27 +0300

    refactor: some changes in common

--- tlparser/src/common/utils/__init__.py
+++ tlparser/src/common/utils/__init__.py
@@ -3,7 +3,10 @@ __init__.py: File, containing other util modules to simplify import.
 """
 
 
-from common.utils.decorators import ReadOnlyClassProperty, Singleton
+from common.utils.decorators import (
+    ReadOnlyClassProperty,
+    Singleton,
+)
 
 
 __all__: list[str] = [
