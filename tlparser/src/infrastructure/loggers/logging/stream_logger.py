3ecacf5902e8798fb7f78420fd9a26f3fae5400d
Twelve <kana.suzucki@gmail.com>
2024-03-13 10:22:09 +0300

    change logger

--- tlparser/src/infrastructure/loggers/logging/stream_logger.py
+++ tlparser/src/infrastructure/loggers/logging/stream_logger.py
@@ -8,32 +8,10 @@ from common.interfaces import ILogger
 
 
 class StreamLogger(ILogger):
-    """
-    StreamLogger: Class, that represents stream logger.
-
-    Args:
-        ILogger: Logger interface.
-    """
-
     def __init__(self, name: str = 'StreamLogger') -> None:
-        """
-        __init__: Initialize stream logger.
-
-        Args:
-            name (str): Name of the logger.
-        """
-
         self._configure_logger(name)
 
     def _configure_logger(self, name: str) -> None:
-        """
-        _configure_logger: Configure logger.
-        It configure logger to write messages to output stream using special string format.
-
-        Args:
-            name (str): Name of the logger.
-        """
-
         self._logger: Logger = getLogger(name)
         self._logger.setLevel(INFO)
 
@@ -47,13 +25,17 @@ class StreamLogger(ILogger):
         stream_handler.setFormatter(formatter)
         self._logger.addHandler(stream_handler)
 
-    def log(self, level: int, message: str) -> None:
-        """
-        log: Log message.
+    def info(self, message: str) -> None:
+        self._logger.info(message)
 
-        Args:
-            level (str): Message level.
-            message (str): Message.
-        """
+    def debug(self, message: str) -> None:
+        self._logger.debug(message)
 
-        self._logger.log(level, message)
+    def warning(self, message: str) -> None:
+        self._logger.warning(message)
+
+    def error(self, message: str) -> None:
+        self._logger.error(message)
+
+    def critical(self, message: str) -> None:
+        self._logger.critical(message)
