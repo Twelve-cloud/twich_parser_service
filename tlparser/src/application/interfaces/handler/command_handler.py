30f058cf2894d4b7bdb42f42c54ead34a75ffb1c
Twelve <kana.suzucki@gmail.com>
2024-03-27 13:37:56 +0300

    refactor: some changes

--- tlparser/src/application/interfaces/handler/command_handler.py
+++ tlparser/src/application/interfaces/handler/command_handler.py
@@ -7,10 +7,10 @@ from abc import ABC as Interface
 from abc import abstractmethod
 from typing import Generic
 from application.commands import C
-from application.dto import Result
+from application.dto import ResultDTO
 
 
 class ICommandHandler(Interface, Generic[C]):
     @abstractmethod
-    async def handle(self, command: C) -> Result:
+    async def handle(self, command: C) -> ResultDTO:
         raise NotImplementedError
