01c8ad91169b5acd0ec6b6b90894b13169910389
Twelve <kana.suzucki@gmail.com>
2024-03-28 19:29:09 +0300

    some changes

--- tlparser/src/presentation/api/rest/v1/responses/success.py
+++ tlparser/src/presentation/api/rest/v1/responses/success.py
@@ -11,5 +11,6 @@ from presentation.api.rest.v1.schemas import JSONAPIObjectSchema
 
 class JSONAPISuccessResponseSchema(ResponseSchema):
     data: Sequence[JSONAPIObjectSchema] = Field(description='List of objects.')
+    included: Optional[Sequence[JSONAPIObjectSchema]] = Field(default=None, description='Included.')
     meta: Optional[dict] = Field(default=None, description='JSON-API metadata.')
     jsonapi: Optional[float] = Field(default=1.1, description='JSON-API version.')
